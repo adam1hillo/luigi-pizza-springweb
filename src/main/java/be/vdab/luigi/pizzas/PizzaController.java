69d6986c4a948310e61569e2f29e4cdef3e0fbbd
Adam Hillo <adam1hillo@gmail.com>
2024-04-12 15:04:14 +0200

    DTO

--- src/main/java/be/vdab/luigi/pizzas/PizzaController.java
+++ src/main/java/be/vdab/luigi/pizzas/PizzaController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 @RestController
@@ -19,8 +20,14 @@ class PizzaController {
         return pizzaService.findAantal();
     }
     @GetMapping("pizzas/{id}")
-    Pizza findById(@PathVariable long id) {
+    IdNaamPrijs findById(@PathVariable long id) {
         return pizzaService.findById(id)
+                .map(IdNaamPrijs::new)
                 .orElseThrow(() -> new PizzaNietGevondenException(id));
+    }
+    private record IdNaamPrijs(long id, String naam, BigDecimal prijs) {
+        IdNaamPrijs(Pizza pizza) {
+            this(pizza.getId(), pizza.getNaam(), pizza.getPrijs());
+        }
     }
 }
